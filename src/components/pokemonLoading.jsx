d257f07acb43b3dd60bb4d36d530b1917a3ab519
Vladimir Lobko <vova1234lobko@gmail.com>
2023-12-13 16:37:46 +0300

    Add Loading

---
+++ src/components/pokemonLoading.jsx
@@ -0,0 +1,7 @@
+export default function PokemonLoading(){
+    return (
+        <div>
+            Loading
+        </div>
+    );
+}
--- src/pages/pokemon.jsx
+++ src/pages/pokemon.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {Suspense, useEffect, useState} from "react";
 import {
     BrowserRouter as Router,
     Route,
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import {getPokemonData} from "../api/pokemonData";
 import Layout from "../components/layout";
+import PokemonLoading from "../components/pokemonLoading";
 
 function Pokemon(){
 
@@ -14,13 +15,16 @@ function Pokemon(){
 
     const [pokemonData,setPokemonData] = useState([]);
     const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         async function fetchData() {
             try {
                 const data = await getPokemonData(id);
                 setPokemonData(data);
                 document.title = "Pokemon PWA - " + data.name;
+                setIsLoading(false);
             } catch (error) {
                 // If the request fails (404 or other errors), update the state with the error
                 setError("Pokemon not found");
@@ -41,57 +45,60 @@ function Pokemon(){
 
     return (
         <Layout>
-
-            <Link
-                to={"/catalog"}
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-                Back
-            </Link>
-            <h1 className="text-4xl">{pokemonData.name}</h1>
-            <img style={{height: '300px', width: '300px'}} alt={pokemonData.name}
-                 src={`https://img.pokemondb.net/artwork/large/${pokemonData.name}.jpg`}/>
-            <div className="my-5">
-                <div className="font-bold">Base info</div>
-                <div className="my-2">Base experience: {pokemonData.base_experience}</div>
-                <div className="my-2">Weight: {pokemonData.weight}</div>
-                <div className="my-2">Height: {pokemonData.height}</div>
-            </div>
-            <div className="my-5">
-                <div className="font-bold">Abilities</div>
-                {pokemonData.abilities?.map((poke, i) => {
-                    return (
-                        <div key={i} className="my-2">
-                            <div>Name: {poke.ability.name}</div>
-                            <div>Slot: {poke.slot}</div>
-                        </div>
-                    );
-                })}
-            </div>
-            <div className="my-5">
-                <div className="font-bold">Moves</div>
-                {pokemonData.moves?.map((poke, i) => {
-                    return (
-                        <div key={i} className="my-3">
-                            <div>Name: {poke.move.name}</div>
-                            <div>Level learned at: {poke.version_group_details[0].level_learned_at}</div>
-                            <div>Move learn method: {poke.version_group_details[0].move_learn_method.name}</div>
-                            <div>Version group: {poke.version_group_details[0].version_group.name}</div>
-                        </div>
-                    );
-                })}
-            </div>
-            <div className="my-5">
-                <div className="font-bold">Stats</div>
-                {pokemonData.stats?.map((poke, i) => {
-                    return (
-                        <div key={i} className="my-3">
-                            <div>Name: {poke.stat.name}</div>
-                            <div>Base stat: {poke.base_stat}</div>
-                        </div>
-                    );
-                })}
-            </div>
+            {isLoading ? <p>Loading...</p> : (
+            <>
+                <Link
+                    to={"/catalog"}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                    Back
+                </Link>
+                <h1 className="text-4xl">{pokemonData.name}</h1>
+                <img style={{height: '300px', width: '300px'}} alt={pokemonData.name}
+                     src={`https://img.pokemondb.net/artwork/large/${pokemonData.name}.jpg`}/>
+                <div className="my-5">
+                    <div className="font-bold">Base info</div>
+                    <div className="my-2">Base experience: {pokemonData.base_experience}</div>
+                    <div className="my-2">Weight: {pokemonData.weight}</div>
+                    <div className="my-2">Height: {pokemonData.height}</div>
+                </div>
+                <div className="my-5">
+                    <div className="font-bold">Abilities</div>
+                    {pokemonData.abilities?.map((poke, i) => {
+                        return (
+                            <div key={i} className="my-2">
+                                <div>Name: {poke.ability.name}</div>
+                                <div>Slot: {poke.slot}</div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="my-5">
+                    <div className="font-bold">Moves</div>
+                    {pokemonData.moves?.map((poke, i) => {
+                        return (
+                            <div key={i} className="my-3">
+                                <div>Name: {poke.move.name}</div>
+                                <div>Level learned at: {poke.version_group_details[0].level_learned_at}</div>
+                                <div>Move learn method: {poke.version_group_details[0].move_learn_method.name}</div>
+                                <div>Version group: {poke.version_group_details[0].version_group.name}</div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="my-5">
+                    <div className="font-bold">Stats</div>
+                    {pokemonData.stats?.map((poke, i) => {
+                        return (
+                            <div key={i} className="my-3">
+                                <div>Name: {poke.stat.name}</div>
+                                <div>Base stat: {poke.base_stat}</div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </>
+            )}
         </Layout>
 
     );
